58322e3471cbd105570aa8eb9f86a6d0d13675a3
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-28 09:22:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+$options = new Options();
+$options->set('defaultFont', 'Calibri');
+$dompdf = new Dompdf($options);
+
+$content=file_get_contents('page.html');
+
+$name=@$_GET['name'];
+$course=@$_GET['course'];
+$score=@$_GET['score'];
+
+$search=['{name}','{score}','{course}'];
+$replaced=[$name,$score,$course];
+$new_content=str_replace($search,$replaced,$content);
+
+
+$dompdf->loadHtml($new_content);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream('test.pdf',[
+    'Attachment'=>0
+]);
