de91a7cf36b2ceaa43247abbc7f26cfd43512f53
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-29 18:14:12 +0700

    test index.php

--- index.php
+++ index.php
@@ -2,33 +2,35 @@
 
 require 'vendor/autoload.php';
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
+echo 'masuk';
 
-$options = new Options();
-$options->set('defaultFont', 'Calibri');
-$dompdf = new Dompdf($options);
+// use Dompdf\Dompdf;
+// use Dompdf\Options;
 
-$content=file_get_contents('page.html');
+// $options = new Options();
+// $options->set('defaultFont', 'Calibri');
+// $dompdf = new Dompdf($options);
 
-$name=@$_GET['name'];
-$course=@$_GET['course'];
-$score=@$_GET['score'];
+// $content=file_get_contents('page.html');
 
-$search=['{name}','{score}','{course}'];
-$replaced=[$name,$score,$course];
-$new_content=str_replace($search,$replaced,$content);
+// $name=@$_GET['name'];
+// $course=@$_GET['course'];
+// $score=@$_GET['score'];
+
+// $search=['{name}','{score}','{course}'];
+// $replaced=[$name,$score,$course];
+// $new_content=str_replace($search,$replaced,$content);
 
 
-$dompdf->loadHtml($new_content);
+// $dompdf->loadHtml($new_content);
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+// // (Optional) Setup the paper size and orientation
+// $dompdf->setPaper('A4', 'landscape');
 
-// Render the HTML as PDF
-$dompdf->render();
+// // Render the HTML as PDF
+// $dompdf->render();
 
-// Output the generated PDF to Browser
-$dompdf->stream('test.pdf',[
-    'Attachment'=>0
-]);
+// // Output the generated PDF to Browser
+// $dompdf->stream('test.pdf',[
+//     'Attachment'=>0
+// ]);
