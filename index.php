09efdf10f1ef71bbc876361c56bad72b448da49a
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-02-29 19:02:39 +0700

    comment loadHTML

--- index.php
+++ index.php
@@ -25,10 +25,10 @@ $new_content=str_replace($search,$replaced,$content);
 $dompdf->loadHtml($new_content);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+// $dompdf->setPaper('A4', 'landscape');
 
-// // Render the HTML as PDF
- $dompdf->render();
+// // // Render the HTML as PDF
+//  $dompdf->render();
 
 // // Output the generated PDF to Browser
 // $dompdf->stream('test.pdf',[
